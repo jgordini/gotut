dc27244bf1b319fe4d766ea4e9709af3a6303aa3
jgordini <jgordini@uab.edu>
2023-11-30 19:31:03 -0600

    first

--- main.go
+++ main.go
@@ -1,19 +1,50 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
-	"github.com/gorilla/mux"
-	"net/http"
+	_ "github.com/mattn/go-sqlite3"
+	"log"
+	"time"
 )
 
 func main() {
-	r := mux.NewRouter()
-	r.HandleFunc("/books/{title}/page/{page}", func(w http.ResponseWriter, r *http.Request) {
-		vars := mux.Vars(r)
-		title := vars["title"]
-		page := vars["page"]
-
-		fmt.Fprintf(w, "you requested book %s on page %s", title, page)
-	})
-	http.ListenAndServe(":80", r)
+	db, err := sql.Open("sqlite3", "./gotut.db")
+	query := `
+		CREATE TABLE IF NOT EXISTS users (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			username TEXT NOT NULL,
+			password TEXT NOT NULL,
+			created_at DATETIME,
+			PRIMARY KEY (id)
+			);`
+	_, err = db.Exec(query)
+	if err != nil {
+		log.Fatal(err)
+	}
+	username := "hogdoe"
+	password := "secret"
+	createdAt := time.Now()
+	result, err := db.Exec("INSERT INTO users (username,password, created_at) VALUES (?, ?, ?)", username, password, createdAt)
+	if err != nil {
+		log.Fatal(err)
+	}
+	userID, err := result.LastInsertId()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(userID)
+	{
+		var (
+			id        int
+			username  string
+			password  string
+			createdAt time.Time
+		)
+		query := `SELECT id, username, password, created_at FROM users WHERE id = ?`
+		err := db.QueryRow(query, 1).Scan(&id, &username, &password, &createdAt)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
 }
